b8f185727a080711ee539ec1f8ef12756c330b86
Shivank Sharma <shivankjaiwal5555@gmail.com>
2024-05-13 17:45:18 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+{/*All the CSS is written in index.css file*/}
 import IdCard from './components/IdCard';
 
 function App() {
--- src/components/IdCard.js
+++ src/components/IdCard.js
@@ -1,50 +1,48 @@
-{/*All the CSS is written in the index.css file */}
 import React from 'react'
 import { useState, useEffect } from 'react'
-import axios from 'axios';
 
 const IdCard = () => {
 
 
-    const [data, setData] = useState([]);
-
+    const [userData, setUserData] = useState([]);
 
     useEffect(() => {
-        axios.get('https://randomuser.me/api/?page=1&results=1&seed=abc')
-            .then(res => {
-                console.log(res)
-                setData(res.data)
+        fetch('https://randomuser.me/api/?results=1')
+            .then(response => response.json())
+            .then(data => {
+                setUserData(data.results);
             })
-            .catch(err => {
-                console.log(err)
+            .catch(error => {
+                console.log('Error Data can not be fetched : ', error);
             })
     }, []);
-
 
 
     return (
         <>
             <div className='container'>
-                {data && <div className='box'>
-                    <img src={`${data.results[0].picture.large}`} alt='profile_1' />
-                    <div className='heading'>
-                        <h2>Name:{`${data.results[0].name.title} ${data.results[0].name.first} ${data.results[0].name.last}`}</h2>
-                        <h4>Email: {`${data.results[0].email}`}</h4>
-                        <h4>Gender: {`${data.results[0].gender}`}</h4>
-                        <h4>Address: {`${data.results[0].location.city} , ${data.results[0].location.state} , ${data.results[0].location.country}`}</h4>
-                        <h4>Street: {`${data.results[0].location.street.number} , ${data.results[0].location.street.name}`}</h4>
-                        <h4>PostCode:  {`${data.results[0].location.postcode}`}</h4>
-                        <h4>Cell:  {`${data.results[0].cell}`}</h4>
-                        <h4>Phone:  {`${data.results[0].phone}`}</h4>
-                        <h4>Registered:  {`${data.results[0].registered.date}`}</h4>
-                        <h4>Age:  {`${data.results[0].registered.age}`}</h4>
-                    </div>
-                </div>
-                }
-                {!data && (
-                    <div >  Loading...</div>
+                {userData.length > 0 ? (
+                    userData.map(user => (
+                        <div className='box' key={user.login.uuid}>
+                            <img src='user.picture.large' alt='user_image' />
+                            <div className='heading'>
+                                <h2>Name:{user.name.first} {user.name.last}</h2>
+                                <h4>Email: {user.email}</h4>
+                                <h4>Gender: {user.gender}</h4>
+                                <h4>Address: {user.location.city}  {user.location.state} {user.location.country}</h4>
+                                <h4>Street: {user.location.street.number} {user.location.street.name}</h4>
+                                <h4>PostCode:  {user.location.postcode}</h4>
+                                <h4>Cell:  {user.cell}</h4>
+                                <h4>Phone:  {user.phone}</h4>
+                                <h4>Registered:  {user.registered.date}</h4>
+                                <h4>Age:  {user.registered.age}</h4>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p>Please wait..............</p>
                 )}
-            </div>
+           </div>
         </>
     )
 }
